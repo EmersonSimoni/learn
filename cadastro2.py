76385f1c5553973c9a6948a94f69029b1839baab
EmersonSimoni <140109668+EmersonSimoni@users.noreply.github.com>
2023-07-20 17:04:03 -0300

    Create main.py

--- cadastro2.py
+++ cadastro2.py
@@ -25,13 +25,17 @@ def buscar(pessoas):
     for pessoa in pessoas:
         identificador, nome, idade = pessoa
         if identificador == identificador_desejado:
+            print(f'')
             print(f'Nome: {nome}')
             print(f'idade: {idade}')
             print(f'id: {identificador}')
             print(f'')
             break
     else:
-        print(f'Pessoa com id {identificador_desejado} não encontrada')
+        print(f'')
+        print(f'Pessoa com ID {identificador_desejado} não encontrada!')
+        print(f'')
+
 
 def main():
     pessoas = []
