94a386449355abe78adbae1cd0266664c6b63fcc
EmersonSimoni <140109668+EmersonSimoni@users.noreply.github.com>
2023-07-20 16:36:34 -0300

    Create main.py

--- cadastro.py
+++ cadastro.py
@@ -1,11 +1,10 @@
 #print("nome",nome_pessoa,"idade",idade_pessoa,"id",id_pessoa)
-
 listas = [[]] #uma lista de lista
 
 while True:
-    print("1-Cadastrar pessoa: ")
-    print("2-Lista Cadastros: ")
-    print("3-Procurar Pessoa Especifica: ")
+    print("1-Cadastrar pessoa. ")
+    print("2-Lista Cadastros. ")
+    print("3-Procurar Pessoa Especifica. ")
     op = int(input())#Escolha da opcao
     if op == 1:
         nova = [] # cria uma lista para adicionar o id, nome e idade da pessoa
@@ -23,36 +22,4 @@ while True:
                 print(mostrar2)#mostra tudo dentro da
 
     elif op == 3:
-        print("pensando")
-
-
-def exibir_menu():
-    print('''Escolha uma opção:
-
-    1. Cadastrar uma pessoa
-    2. Listar pessoas cadastradas
-    3. Procurar dados de uma pessoa
-    ''')
-
-    def cadastrar(pessoas):
-        identificador = input('Id? ')
-        nome = input('Nome? ')
-        idade = int(input('Idade? '))
-        pessoas.append((identificador, nome, idade))
-
-        def listar(pessoas):
-            for pessoa in pessoas:
-                identificador, nome, idade = pessoa
-                print(f'Nome: {nome}, idade: {idade}, id: {identificador}')
-
-
-def buscar(pessoas):
-    identificador_desejado = input('Id? ')
-    for pessoa in pessoas:
-        identificador, nome, idade = pessoa
-        if identificador == identificador_desejado:
-            print(f'Nome: {nome}, idade: {idade}, id: {identificador}')
-            break
-    else:
-        print(f'Pessoa com id {identificador_desejado} não encontrada')
-
+        print("Buscando...")
