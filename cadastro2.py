cab06336b77db3ab3c67bcbfdf4b9919b9d4532d
EmersonSimoni <140109668+EmersonSimoni@users.noreply.github.com>
2023-07-20 16:42:32 -0300

    Create main.py

--- cadastro2.py
+++ cadastro2.py
@@ -6,38 +6,40 @@ def exibir_menu():
     3. Procurar dados de uma pessoa
     ''')
 
-    def main():
-        pessoas = []
+def cadastrar(pessoas):
+    identificador = input('Id? ')
+    nome = input('Nome? ')
+    idade = int(input('Idade? '))
+    pessoas.append((identificador, nome, idade))
 
-        while True:
-            exibir_menu()
-            opcao = int(input('Opção? '))
-            if opcao == 1:
-                cadastrar(pessoas)
-            elif opcao == 2:
-                listar(pessoas)
-            elif opcao == 3:
-                buscar(pessoas)
-            else:
-                print('Opção inválida')
+def listar(pessoas):
+    for pessoa in pessoas:
+        identificador, nome, idade = pessoa
+        print(f'Nome: {nome}, idade: {idade}, id: {identificador}')
 
-    def cadastrar(pessoas):
-        identificador = input('Id? ')
-        nome = input('Nome? ')
-        idade = int(input('Idade? '))
-        pessoas.append((identificador, nome, idade))
-
-    def listar(pessoas):
-        for pessoa in pessoas:
-            identificador, nome, idade = pessoa
+def buscar(pessoas):
+    identificador_desejado = input('Id? ')
+    for pessoa in pessoas:
+        identificador, nome, idade = pessoa
+        if identificador == identificador_desejado:
             print(f'Nome: {nome}, idade: {idade}, id: {identificador}')
+            break
+    else:
+        print(f'Pessoa com id {identificador_desejado} não encontrada')
 
-    def buscar(pessoas):
-        identificador_desejado = input('Id? ')
-        for pessoa in pessoas:
-            identificador, nome, idade = pessoa
-                if identificador == identificador_desejado:
-                    print(f'Nome: {nome}, idade: {idade}, id: {identificador}')
-                    break
-                else:
-                    print(f'Pessoa com id {identificador_desejado} não encontrada')
+def main():
+    pessoas = []
+
+    while True:
+        exibir_menu()
+        opcao = int(input('Opção? '))
+        if opcao == 1:
+            cadastrar(pessoas)
+        elif opcao == 2:
+            listar(pessoas)
+        elif opcao == 3:
+            buscar(pessoas)
+        else:
+            print('Opção inválida')
+
+main()
